16118a0be0832f36bd9106bd7e3d93baee1d4935
John Watson <john.watson1@defra.gov.uk>
2023-11-25 16:16:58 +0000

    Update auth

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,10 @@
+const { USER } = require('../auth/scopes')
 const { GET } = require('../constants/http-verbs')
 
 module.exports = {
   method: GET,
   path: '/',
-  options: {
-    auth: false
-  },
+  options: { auth: { strategy: 'jwt', scope: [USER] } },
   handler: (request, h) => {
     return h.view('index')
   }
