3073a5f1fe41633a069cc218c61c1f92c8e1a703
John Watson <john.watson1@defra.gov.uk>
2023-12-07 10:27:03 +0000

    Add log

--- app/auth/get-public-key.js
+++ app/auth/get-public-key.js
@@ -3,6 +3,7 @@ const { serverConfig } = require('../config')
 
 const getPublicKey = async () => {
   const { payload } = await Wreck.get(`${serverConfig.authHost}/auth/public-key`, { json: true })
+  console.log('public key payload', payload)
   return payload
 }
 
